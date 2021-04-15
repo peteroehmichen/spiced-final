230c4e0ca497ecdafef5a343e20a05424b1df2d9
Peter Oehmichen <peter.oehmichen@mailbox.org>
2021-04-15 11:39:23 +0200

    changed to res.send()

--- server/loggedOutRoutes.js
+++ server/loggedOutRoutes.js
@@ -134,7 +134,28 @@ router.get("/welcome/oauth", (req, res) => {
 });
 
 router.get("/welcome/callback", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "callback.html"));
+    // res.sendFile(path.join(__dirname, "..", "client", "callback.html"));
+    res.send(`
+        <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <p>please wait...</p>
+        <script>
+            if (window.opener) {
+                window.opener.postMessage({ oauth: window.location.search });
+                window.close();
+            }
+        </script>
+    </body>
+</html>
+
+    `);
 });
 
 router.post("/welcome/reset.json", async (req, res) => {
